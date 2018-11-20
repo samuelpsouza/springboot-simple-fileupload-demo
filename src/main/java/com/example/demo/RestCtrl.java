4a1b2c14e8d68d3d85dbddd10e7edb0853b0d90f
Samuel <samuelsouza.contact@gmail.com>
2018-11-19 23:00:29 -0200

    first commit

---
+++ src/main/java/com/example/demo/RestCtrl.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@RestController
+@Slf4j
+public class RestCtrl {
+    @PostMapping("/fileupload")
+    public ResponseEntity<Object> upload(@RequestParam("file") MultipartFile file) {
+        return ResponseEntity.ok().body(new Object());
+    }
+}
